23b0c562b78c392b52d53ed65cabee94a6e9f07b
Erwan Vinet <erwanvinet034@gmail.com>
2024-05-27 15:10:33 +0200

    models version 2

--- tryffle/tryffleApp/admin.py
+++ tryffle/tryffleApp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from tryffleApp.models import DocumentPdf, Page
 # Register your models here.
+
+class DocumentPdfAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date')
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('document', 'number')
+
+admin.site.register(DocumentPdf, DocumentPdfAdmin)
+admin.site.register(Page, PageAdmin)
