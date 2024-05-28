5cf4d2393c088bd9102f3f9b84eacae3639dc5f5
Erwan Vinet <erwanvinet034@gmail.com>
2024-05-28 12:00:16 +0200

    docker v2 + modification settings + nouvel migration

--- tryffle/tryffle/settings.py
+++ tryffle/tryffle/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'tryffle.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tryffle',
+        'USER': 'tryffle',
+        'PASSWORD': 'secret',
+        'HOST': 'tryffle_postgres',
+        'PORT': '5432',
     }
 }
 
