4dd05c07e61d4780b4241a25b2ce301530d0d5d5
Erwan Vinet <erwanvinet034@gmail.com>
2024-05-29 15:51:15 +0200

    configuration de celery et rabbitMQ + premier test

--- tryffle/tryffle/settings.py
+++ tryffle/tryffle/settings.py
@@ -130,3 +130,11 @@ MEDIA_URL = 'media/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CELERY_BROKER_URL = 'amqp://guest:guest@rabbitmq:5672//'
+CELERY_RESULT_BACKEND = 'rpc://'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'UTC'
