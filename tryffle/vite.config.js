d51324dbd36dafd4158a35224b1e45152c8f0c3b
TROUVERIE Joachim <jtrouverie@joakode.fr>
2024-06-07 15:09:53 +0200

    minimal patch

--- tryffle/vite.config.js
+++ tryffle/vite.config.js
@@ -3,18 +3,18 @@ import { resolve } from "path";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  root: resolve("/app/static/src"),
-  base: "/app/static/",
+  root: resolve("./static/src"),
+  base: "/static/",
   plugins: [vue()],
   build: {
-    outDir: resolve("/app/static/manifest.json"),
+    outDir: resolve("./static/dist"),
     assetsDir: "",
     manifest: true,
     emptyOutDir: true,
     rollupOptions: {
       // Overwrite default .html entry to main.ts in the static directory
       input:{
-        main: resolve("/app/static/src/js/main.js"),
+        main: resolve("./static/src/main.js"),
       } 
     },
   },
